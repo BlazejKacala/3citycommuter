d5f7ff9df5aeb9b753f84304f7fcc3f737f0fb27
Blazej Kacala <blazej.kacala@gnb.pl>
2024-01-11 21:58:22 +0100

    Use collect as state to enable navbar update

--- app/src/main/java/pl/bkacala/threecitycommuter/ui/nav/Extensions.kt
+++ app/src/main/java/pl/bkacala/threecitycommuter/ui/nav/Extensions.kt
@@ -1,10 +1,13 @@
 package pl.bkacala.threecitycommuter.ui.nav
 
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.currentBackStackEntryAsState
 
+@Composable
 fun NavController.isTopLevelDestinationInHierarchy(destination: TopLvlDestinations) =
-    this.currentDestination?.hierarchy?.any {
+    this.currentBackStackEntryAsState().value?.destination?.hierarchy?.any {
         it.route?.contains(destination.name, true) ?: false
     } ?: false
