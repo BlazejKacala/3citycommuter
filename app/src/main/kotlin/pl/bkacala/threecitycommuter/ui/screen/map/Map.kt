db21cd75ae2eafc04f4acec4317149741c58f962
Blazej Kacala <blazej.kacala@gnb.pl>
2024-01-31 21:44:24 +0100

    Hoist state in maps

--- app/src/main/kotlin/pl/bkacala/threecitycommuter/ui/screen/map/Map.kt
+++ app/src/main/kotlin/pl/bkacala/threecitycommuter/ui/screen/map/Map.kt
@@ -6,14 +6,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toBitmap
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.CameraPositionState
@@ -21,32 +17,18 @@ import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapsComposeExperimentalApi
 import com.google.maps.android.compose.clustering.Clustering
-import com.google.maps.android.compose.clustering.rememberClusterManager
 import pl.bkacala.threecitycommuter.R
 import pl.bkacala.threecitycommuter.ui.common.UiState
 import pl.bkacala.threecitycommuter.ui.screen.map.component.BusStopMapItem
-import pl.bkacala.threecitycommuter.utils.changeColor
 
 @OptIn(MapsComposeExperimentalApi::class)
 @Composable
 fun Map(
     cameraPositionState: CameraPositionState,
-    viewModel: MapScreenViewModel,
+    busStops: List<BusStopMapItem>,
     onBusStationSelected: (busStation: BusStopMapItem) -> Unit,
 ) {
     val context = LocalContext.current
-    val color = MaterialTheme.colorScheme.primary.toArgb()
-    val iconBitmap = remember {
-        ContextCompat.getDrawable(context, R.drawable.bus_station)?.toBitmap()?.changeColor(color)
-    }
-
-    LaunchedEffect(viewModel) {
-        viewModel.location.collect {
-            cameraPositionState.move(
-                CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude, it.longitude), 16.0f)
-            )
-        }
-    }
 
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
@@ -58,39 +40,9 @@ fun Map(
             )
         )
     ) {
-        val busIcon = remember {
-            BitmapDescriptorFactory.fromBitmap(iconBitmap!!)
-        }
-
-        val clusterManager = rememberClusterManager<BusStopMapItem>()
-        val clusterRenderer = rememberBusStopsClusterRenderer(
-            clusterManager = clusterManager,
-            itemBitmap = busIcon
+        Clustering(
+            items = busStops,
         )
-        clusterManager?.setOnClusterItemClickListener {
-            onBusStationSelected(it)
-            false
-        }
-
-        if (clusterManager != null && clusterRenderer != null) {
-            clusterManager.renderer = clusterRenderer
-
-            when (val busStops = viewModel.busStops.collectAsState().value) {
-                is UiState.Error -> {
-
-                }
-
-                UiState.Loading -> {
-                }
-
-                is UiState.Success -> {
-                    Clustering(
-                        items = busStops.data,
-                        clusterManager = clusterManager
-                    )
-                }
-            }
-        }
     }
 }
 
--- app/src/main/kotlin/pl/bkacala/threecitycommuter/ui/screen/map/MapScreen.kt
+++ app/src/main/kotlin/pl/bkacala/threecitycommuter/ui/screen/map/MapScreen.kt
@@ -3,28 +3,33 @@ package pl.bkacala.threecitycommuter.ui.screen.map
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.rememberCameraPositionState
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
+import pl.bkacala.threecitycommuter.ui.common.UiState
+import pl.bkacala.threecitycommuter.ui.screen.map.component.BusStopMapItem
 import pl.bkacala.threecitycommuter.ui.screen.map.component.DeparturesBottomSheet
 
 @OptIn(FlowPreview::class)
 @Composable
 fun MapScreen() {
-
     BoxWithConstraints {
 
         val viewModel = hiltViewModel<MapScreenViewModel>()
         val cameraPositionState = rememberCameraPositionState()
 
-
         LaunchedEffect(cameraPositionState) {
             snapshotFlow { cameraPositionState.position.target }
-                .debounce(100)
+                .debounce(200)
                 .distinctUntilChanged()
                 .collect {
                     cameraPositionState.projection?.visibleRegion?.let {
@@ -33,14 +38,34 @@ fun MapScreen() {
                 }
         }
 
+        LaunchedEffect(viewModel) {
+            viewModel.location.collect {
+                cameraPositionState.move(
+                    CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude, it.longitude), 16.0f)
+                )
+            }
+        }
+
+        val busStops = viewModel.busStops.collectAsState().value
+        val busStopsState = remember(viewModel) { mutableStateOf(emptyList<BusStopMapItem>()) }
+        when(busStops) {
+            is UiState.Error -> {
+                //TODO
+            }
+            UiState.Loading -> {
+                //TODO
+            }
+            is UiState.Success -> {
+                busStopsState.value = busStops.data
+            }
+        }
+
         Map(
             cameraPositionState = cameraPositionState,
-            viewModel = viewModel,
+            busStops = busStopsState.value,
             onBusStationSelected = { viewModel.onBusStopSelected(it) }
         )
         DeparturesBottomSheet(departures = viewModel.departures.collectAsState().value)
-
-
     }
 
 }
