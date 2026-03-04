ff27387c4c0dcb86efcf06d2564f57b92236adc9
Blazej Kacala <blazej.kacala@gnb.pl>
2026-03-04 22:39:05 +0100

    Add test made by claude.

--- app/src/test/java/pl/bkacala/threecitycommuter/mocks/MockPermissionFlows.kt
+++ app/src/test/java/pl/bkacala/threecitycommuter/mocks/MockPermissionFlows.kt
@@ -7,38 +7,34 @@ import dev.shreyaspatil.permissionFlow.PermissionState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-object  MockPermissionFlows {
-    val mockGrantedPermissionFlow = object: PermissionFlow {
-        override fun getMultiplePermissionState(vararg permissions: String): StateFlow<MultiplePermissionState> {
-            throw IllegalStateException("It's not used and therefore shouldn't be called")
+object MockPermissionFlows {
+    val mockGrantedPermissionFlow =
+        object : PermissionFlow {
+            override fun getMultiplePermissionState(vararg permissions: String): StateFlow<MultiplePermissionState> =
+                throw IllegalStateException("It's not used and therefore shouldn't be called")
+
+            override fun getPermissionState(permission: String): StateFlow<PermissionState> =
+                MutableStateFlow(PermissionState(Manifest.permission.ACCESS_FINE_LOCATION, true, true))
+
+            override fun notifyPermissionsChanged(vararg permissions: String) {}
+
+            override fun startListening() {}
+
+            override fun stopListening() {}
         }
 
-        override fun getPermissionState(permission: String): StateFlow<PermissionState> {
-            return MutableStateFlow(PermissionState(Manifest.permission.ACCESS_FINE_LOCATION, true))
+    val mockDeniedPermissionFlow =
+        object : PermissionFlow {
+            override fun getMultiplePermissionState(vararg permissions: String): StateFlow<MultiplePermissionState> =
+                throw IllegalStateException("It's not used and therefore shouldn't be called")
+
+            override fun getPermissionState(permission: String): StateFlow<PermissionState> =
+                MutableStateFlow(PermissionState(Manifest.permission.ACCESS_FINE_LOCATION, false, false))
+
+            override fun notifyPermissionsChanged(vararg permissions: String) {}
+
+            override fun startListening() {}
+
+            override fun stopListening() {}
         }
-
-        override fun notifyPermissionsChanged(vararg permissions: String) {}
-
-        override fun startListening() {}
-
-        override fun stopListening() {}
-
-    }
-
-    val mockDeniedPermissionFlow = object: PermissionFlow {
-        override fun getMultiplePermissionState(vararg permissions: String): StateFlow<MultiplePermissionState> {
-            throw IllegalStateException("It's not used and therefore shouldn't be called")
-        }
-
-        override fun getPermissionState(permission: String): StateFlow<PermissionState> {
-            return MutableStateFlow(PermissionState(Manifest.permission.ACCESS_FINE_LOCATION, false))
-        }
-
-        override fun notifyPermissionsChanged(vararg permissions: String) {}
-
-        override fun startListening() {}
-
-        override fun stopListening() {}
-
-    }
 }
